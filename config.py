0f9d79a4707dfdd4c491c5fb7ec2d38a59455088
Yuriy Semesyuk <yuriysemesyuk@MacBook-Pro-Yuriy.local>
2020-02-24 14:31:53 +0200

    psql_commit

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Configuration:
     DEBUG = True
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') #'postgres+psycopg2://yuriysemesyuk:@localhost/test_db'
+    SQLALCHEMY_DATABASE_URI = 'postgres+psycopg2://yuriysemesyuk:@localhost/test_db' #'sqlite:///' + os.path.join(basedir, 'app.db')
     SECRET_KEY = "mysecertkey"
 
     ###flask_security
